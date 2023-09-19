e28afb9d762ad2070d05a19679f8322aec9092fc
Pratik <pru@kanakinfosystems.com>
2023-09-19 19:04:39 +0530

    [ADD]module added subscription_kanak.

---
+++ subscription_kanak/CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+Version 15.0.1.0
+---------------------
+Date: 11-MAY-2022
+---------------------
+- Initial version to release.
---
+++ subscription_kanak/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Powered by Kanak Infosystems LLP.
+# © 2020 Kanak Infosystems LLP. (<https://www.kanakinfosystems.com>).
+
+from . import models
+from . import controllers
+# from . import wizard
+
+from odoo import api, SUPERUSER_ID
+
+
+def _post_init_hook(env):
+    env['ir.config_parameter'].set_param('sale.automatic_invoice', True)
